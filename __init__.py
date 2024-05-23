35306f0f9e8cdac19af502af0d0c9142d78fe830
Leskinkotik <166804321+Leskinkotik@users.noreply.github.com>
2024-05-23 19:10:27 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,8 +9,11 @@ app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'  # Clé secrète pour les sessions
 
 # Fonction pour créer une clé "authentifie" dans la session utilisateur
-def est_authentifie():
-    return session.get('authentifie')
+def est_authentifie_admin():
+    return session.get('authentifie_admin')
+
+def est_authentifie_user():
+    return session.get('authentifie_user')
 
 @app.route('/')
 def hello_world():
@@ -18,7 +21,7 @@ def hello_world():
 
 @app.route('/lecture')
 def lecture():
-    if not est_authentifie():
+    if not est_authentifie_admin():
         # Rediriger vers la page d'authentification si l'utilisateur n'est pas authentifié
         return redirect(url_for('authentification'))
 
@@ -30,9 +33,13 @@ def authentification():
     if request.method == 'POST':
         # Vérifier les identifiants
         if request.form['username'] == 'admin' and request.form['password'] == 'password': # password à cacher par la suite
-            session['authentifie'] = True
+            session['authentifie_admin'] = True
             # Rediriger vers la route lecture après une authentification réussie
             return redirect(url_for('lecture'))
+        if request.form['username'] == 'user' and request.form['password'] == '12345': # password à cacher par la suite
+            session['authentifie_user'] = True
+            # Rediriger vers la route fiche_nom après une authentification réussie
+            return redirect(url_for('fiche_nom'))
         else:
             # Afficher un message d'erreur si les identifiants sont incorrects
             return render_template('formulaire_authentification.html', error=True)
@@ -79,6 +86,10 @@ def enregistrer_client():
 
 @app.route('/fiche_nom/', methods=['GET'])
 def search_client_by_name():
+    if not est_authentifie_admin():
+        # Rediriger vers la page d'authentification si l'utilisateur n'est pas authentifié
+        return redirect(url_for('authentification'))
+        
     name = request.args.get('name')
 
     if not name:
